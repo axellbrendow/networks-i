299f8d788f55a02956f20d6185e5cda9aed1b80e
axell-brendow <breno.axel@gmail.com>
2020-11-02 19:42:47 -0300

    Create Messages class on server side
    This class defines observers for each message type of the client

---
+++ works/udp-prices-system/serverMessages.js
@@ -0,0 +1,63 @@
+const { Observers, MessageType } = require("./Observers");
+const { Repository } = require("./serverRepository");
+
+class Messages {
+  /**
+   * @param {import('dgram').Socket} server
+   * @param {Observers} observers
+   * @param {Repository} repository
+   */
+  constructor(server, observers, repository) {
+    this.server = server;
+    /** @type {{ [id: string]: import("./serverMessages").Message }} */
+    this.messages = {};
+    this.repository = repository;
+    this.observers = observers;
+    this.addObservers();
+  }
+
+  addObservers() {
+    this.observers.addObserver((message, info) => {
+      if (message.type === MessageType.ADD_PRICE) {
+        message.data = this.repository.addPrice(message.data);
+
+        this.server.send(
+          JSON.stringify(message),
+          info.port,
+          "localhost",
+          error => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log("Message sent (response):", message);
+            }
+          }
+        );
+      }
+    });
+
+    this.observers.addObserver((message, info) => {
+      if (message.type === MessageType.SEARCH_PRICE) {
+        message.data = this.repository.searchPrice(message.data);
+
+        if (message.data.price === Number.MAX_SAFE_INTEGER)
+          message.data = "Nenhum posto encontrado";
+
+        this.server.send(
+          JSON.stringify(message),
+          info.port,
+          "localhost",
+          error => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log("Message sent (response):", message);
+            }
+          }
+        );
+      }
+    });
+  }
+}
+
+module.exports.Messages = Messages;
