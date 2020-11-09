5560267d4288bc5d2d48908b8fdba1d4bcc2d66d
axell-brendow <breno.axel@gmail.com>
2020-11-09 08:51:47 -0300

    Create server Observers class

---
+++ works/tcp-naval-battle-game/server/Observers.js
@@ -0,0 +1,61 @@
+const { createNewId } = require("../utils");
+
+/**
+@template MessageData
+
+@typedef {
+  (
+    message: import('../shared/Message')<MessageData>,
+    messages: import('./Messages'),
+    socketName: string
+  ) => void
+} Observer
+ */
+
+class Observers {
+  constructor() {
+    /** @type {{ [observerId: string]: Observer<any> }} */
+    this.observers = {};
+
+    this.createNewId = this.createNewId.bind(this);
+    this.addObserver = this.addObserver.bind(this);
+    this.removeObserver = this.removeObserver.bind(this);
+    this.runObservers = this.runObservers.bind(this);
+  }
+
+  createNewId() {
+    let id = createNewId();
+    while (this.observers[id]) id = createNewId();
+    return id;
+  }
+
+  /**
+   * @param {Observer<any>} observer
+   *
+   * @returns {string} the id of this observer. Use it to remove the observer.
+   */
+  addObserver(observer) {
+    const id = this.createNewId();
+    this.observers[id] = observer;
+    return id;
+  }
+
+  /**
+   * @param {string} id
+   */
+  removeObserver(id) {
+    delete this.observers[id];
+  }
+
+  /**
+   * @param {import('../shared/Message')} message
+   * @param {import('./Messages')} messages
+   * @param {string} socketName
+   */
+  runObservers(message, messages, socketName) {
+    for (const observer of Object.values(this.observers))
+      observer(message, messages, socketName);
+  }
+}
+
+module.exports = Observers;
