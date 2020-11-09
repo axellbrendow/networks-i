e5a9264970e97abe7dc966e172934a3f5f6cc88b
axell-brendow <breno.axel@gmail.com>
2020-11-09 08:50:28 -0300

    Export all Observers

---
+++ works/tcp-naval-battle-game/server/observers/index.js
@@ -0,0 +1,4 @@
+module.exports.createMatch = require("./createMatch");
+module.exports.setBoard = require("./setBoard");
+module.exports.shoot = require("./shoot");
+module.exports.startMatch = require("./startMatch");
