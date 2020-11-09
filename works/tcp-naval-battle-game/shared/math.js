e504affef1437f5e224e256dc4136f53495aec28
axell-brendow <breno.axel@gmail.com>
2020-11-09 08:36:41 -0300

    Add util to get random integer

---
+++ works/tcp-naval-battle-game/shared/math.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} min
+ * @param {number} max
+ */
+module.exports.getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
