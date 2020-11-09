d79db821825cb306869f43aa2cf981cf88d2bed9
axell-brendow <breno.axel@gmail.com>
2020-11-09 08:59:52 -0300

    Create Messages class to send and receive

---
+++ works/tcp-naval-battle-game/client/Messages.js
@@ -0,0 +1,125 @@
+const { createNewId } = require("../utils");
+
+/**
+@typedef {{
+  [messageType in import("../shared/Message").MessageType]: (
+    (
+      id: string,
+      messageType: messageType,
+      data: import('../shared/requests').Requests[messageType],
+      numTries: number,
+    ) => Promise<
+      import('../shared/Message')<
+        import('../shared/responses').Responses[messageType]
+      >
+    >
+  ) & (
+    (
+      id: string,
+      messageType: messageType,
+      data: import('../shared/requests').Requests[messageType],
+    ) => Promise<
+      import('../shared/Message')<
+        import('../shared/responses').Responses[messageType]
+      >
+    >
+  ) 
+}} SendMessageTypes
+*/
+
+/**
+@typedef {{
+  [messageType in import("../shared/Message").MessageType]: (
+    (
+      messageType: messageType,
+      observer: import("./Observers").Observer<
+        import('../shared/responses').Responses[messageType]
+      >,
+    ) => void
+  )
+}} AddObserverTypes
+*/
+
+/**
+@template T
+
+@typedef {
+  (T extends any ? (x: T) => any : never) extends 
+    (x: infer R) => any ? R : never
+} UnionToIntersection
+ */
+
+/**
+@typedef {UnionToIntersection<SendMessageTypes[keyof SendMessageTypes]>} SendMessageFunc
+*/
+
+/**
+@typedef {UnionToIntersection<AddObserverTypes[keyof AddObserverTypes]>} AddObserverFunc
+*/
+
+class Messages {
+  /**
+   * @param {import('net').Socket} client
+   * @param {import('./Observers')} observers
+   */
+  constructor(client, observers) {
+    this.client = client;
+    /** @type {{ [id: string]: import("../shared/Message") }} */
+    this.messages = {};
+    this.observers = observers;
+
+    /** @type {SendMessageFunc} */
+    this.sendMessage = (id, messageType, data, numTries = 3) => {
+      const message = this.createNewMessage(id, data, messageType);
+
+      const messageBuffer = Buffer.from(JSON.stringify(message));
+
+      return new Promise((resolve, reject) => {
+        this.client.write(messageBuffer, error => {
+          if (error) {
+            console.error(error);
+            if (numTries > 0)
+              this.sendMessage(id, data, messageType, numTries - 1)
+                .then(resolve)
+                .catch(reject);
+            else {
+              console.error("Nao foi possivel enviar os dados (3 tentativas)");
+              reject(error);
+            }
+          } else {
+            const id = this.observers.addObserver(msg => {
+              if (msg.id === message.id && msg.type === message.type) {
+                this.observers.removeObserver(id);
+                resolve(msg);
+              }
+            });
+          }
+        });
+      });
+    };
+
+    /** @type {AddObserverFunc} */
+    this.addObserverFor = (messageType, observer) => {
+      this.observers.addObserver((message, messages) => {
+        if (message.type === messageType) observer(message, messages);
+      });
+    };
+
+    this.createNewMessage = this.createNewMessage.bind(this);
+  }
+
+  /**
+   * @template T
+   *
+   * @param {string} id
+   * @param {T} data
+   * @param {import('../shared/Message').MessageType} messageType
+   */
+  createNewMessage(id, data, messageType) {
+    const message = { id: id || createNewId(), type: messageType, data };
+    this.messages[message.id] = message;
+    return message;
+  }
+}
+
+module.exports = Messages;
