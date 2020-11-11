2b8f6c002c8843e52829eab0aa6cf9267031e3f8
axell-brendow <breno.axel@gmail.com>
2020-11-11 19:03:34 -0300

    Add new properties to the Match entity
    I needed to store the player socket name to use it to send
    specific messages for each player.
    Also I needed to create a boolean flag to indicate if player 2 is a bot.

--- works/tcp-naval-battle-game/shared/Match.js
+++ works/tcp-naval-battle-game/shared/Match.js
@@ -14,16 +14,32 @@ class Match {
    * @param {string} id
    * @param {import('./Player')} player1
    * @param {import('./Player')} player2
+   * @param {string} player1SocketName
+   * @param {string} player2SocketName
    * @param {import('./Board')} player1Board
    * @param {import('./Board')} player2Board
    * @param {MatchStatus} status
+   * @param {boolean} player2IsABot
    */
-  constructor(id, player1, player2, player1Board, player2Board, status) {
+  constructor(
+    id,
+    player1,
+    player2,
+    player1SocketName,
+    player2SocketName,
+    player1Board,
+    player2Board,
+    player2IsABot,
+    status
+  ) {
     this.id = id;
     this.player1 = player1;
     this.player2 = player2;
+    this.player1SocketName = player1SocketName;
+    this.player2SocketName = player2SocketName;
     this.player1Board = player1Board;
     this.player2Board = player2Board;
+    this.player2IsABot = player2IsABot;
 
     /** @type {{ [playerId: string]: import('../shared/Board') }} */
     this.boardsByPlayerId = {};
@@ -38,13 +54,16 @@ class Match {
     this.status = status;
 
     this.toJSON = () => ({
-      id,
-      player1,
-      player2,
-      player1Board,
-      player2Board,
+      id: this.id,
+      player1: this.player1,
+      player2: this.player2,
+      player1SocketName: this.player1SocketName,
+      player2SocketName: this.player2SocketName,
+      player1Board: this.player1Board,
+      player2Board: this.player2Board,
+      player2IsABot: this.player2IsABot,
       boardsByPlayerId: this.boardsByPlayerId,
-      status,
+      status: this.status,
     });
   }
 }
